11cb848c0de149095d04ae2c915a088b11ffa2a4
amiranda <amiranda@connectwise.com>
2019-10-09 09:09:11 -0400

    added string dependency

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 //main file for testing git tracking
 
 #include <iostream>
+#include <string>
 
 int main(){
+    std::string testing = "hello world";
     std::cout << "Test" << std::endl;
     return 0;
 }
