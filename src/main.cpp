26743ad92963df0e309031dd1655251c25d83ee9
amiranda <amiranda@connectwise.com>
2019-10-09 10:15:06 -0400

    Added define statement to try out preprocessor

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,11 @@
 #include <iostream>
 #include <string>
 
+#define MESSAGE std::cout << "message text here for failure" << std::endl
+
 int main(){
     std::string testing = "hello world";
     std::cout << "Test" << std::endl;
+    MESSAGE;
     return 0;
 }
